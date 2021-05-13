4dd8d1a58a85289424fcf70877345c1f8b8f572b
mayurpatel78645 <mayurpatel78645@gmail.com>
2021-05-12 23:22:05 -0500

    try to fix overlay

--- app.js
+++ app.js
@@ -1,106 +1,64 @@
-//call API request data
-//parse data w/ .json()
-//return data
-
-//capture input from html form
-//append user input to api call
-//parse resposne
-//insert into dom or render page
-
-
-const movieList = document.querySelector('.movies')
-const search = document.querySelector('.search')
-
-
-//input from event listerner
+const movieList = document.querySelector('.movies');
+const search = document.querySelector('.search');
 
 const dataRequest = async (url) => {
-  const request = await fetch(url)
-  const responseData = await request.json()
-
-  return responseData
+  const request = await fetch(url);
+  const responseData = await request.json();
+  return responseData;
 }
 
 const render = (data) => {
-  console.log(data.Search)
-  movieList.innerHTML = ''
+  movieList.innerHTML = '';
   data.Search.forEach((movie) => {
     movieList.insertAdjacentHTML('beforeend', `
     <li data-id=${movie.imdbID}>
     <div class = 'overlay'>
     <img class='image' src='${movie.Poster === 'N/A' ? movie.Poster = '/images/default_image.jpg' : movie.Poster}'></img>
-    
     </div>
     </li>
-    `
-    )
-}
-)
+    `)}
+  );
 }
 
 const renderHover =  (data) => {
-  console.log(data)
-  const overlay = document.querySelectorAll('.overlay')
-  overlay.innerHTML = ''
-  overlay.forEach(ele => {
-    
-    ele.insertAdjacentHTML('beforeend', `
-    <p>${data.Title}</p>
-    <p>${data.Plot}</p>
-    <p>${data.imdbRating}</p>
-    
-    `
-    )
-
-  })
-  
-  
-  
-  
-
-
+  const overlay = document.querySelectorAll('.overlay');
+  overlay.innerHTML = "";
+    overlay.forEach(element => {
+      element.insertAdjacentHTML('beforeend', `
+      <p>
+        <strong>${data.Title}</strong><br>
+        <em><strong>plot:</strong> ${data.Plot}</em><br>
+        <strong>imdb rating: ${data.imdbRating}</strong><br>
+      </p>
+      `);
+  });
 }
 
-
 function handleEvent(e) {
-  
   if(e.target.nodeName === 'FORM') {
-    
-    e.preventDefault()
-    console.log('event happened')
+    e.preventDefault();
     const url = `http://www.omdbapi.com/?apikey=89a15f2d&s=${e.target.firstElementChild.value}`
     dataRequest(url)
     .then((responseData) => {
-    render(responseData)
-    e.target.firstElementChild.value = ''
-    const image = document.querySelectorAll('.image')
-    image.forEach(ele => {
-      ele.addEventListener('mouseenter', handleHover)
-    })
-    
-
+    render(responseData);
+    e.target.firstElementChild.value = '';
   })
-    
-
+  .then(() => {
+    const image = document.querySelectorAll('.image');
+    image.forEach(ele => {
+      ele.addEventListener('mouseenter', handleHover);
+    });
+  })
   } 
 } 
 
 function handleHover(e) {
-  //console.log(e.target)
   const id = e.target.parentElement.parentElement.dataset.id;
   const url = `http://www.omdbapi.com/?apikey=89a15f2d&i=${id}`;
   dataRequest(url)
   .then(data => {
-    console.log(data.Title, data.imdbRating, data.Plot)
-  renderHover(data)
-
-  }
-    ) ;
-  
-
- }
-
-
-
+    renderHover(data);
+  });
+}
 
 search.addEventListener('submit', handleEvent);
